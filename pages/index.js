a77310f6676567c26af594ac2ad254aa0b521299
rostrae <irenewu222@gmail.com>
2022-01-22 20:05:42 -0800

    header fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Header from "../comps/header/Header";
+import downarrow from '../assets/down-arrow.svg';
 
 export default function Home() {
   return <div className="centered-cont">
     <Header />
+    <img className="down-arrow" src={downarrow.src} />
+    <h1>owehfewfwefw</h1>
+    <h1>owehfewfwefw</h1>
+    <h1>owehfewfwefw</h1>
+    <h1>owehfewfwefw</h1>
+    <h1>owehfewfwefw</h1>
+    <h1>owehfewfwefw</h1>
   </div>
 }
