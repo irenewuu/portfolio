78c5ff1aaeff063faaf964eb12a8e160d4b847d0
rostrae <irenewu222@gmail.com>
2022-01-25 00:56:23 -0800

    link fix again? and more assets added

--- pages/projects/kalm.js
+++ pages/projects/kalm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import WorkHeader from '../../comps/header/WorkHeader';
+import WorkHeader from './../../comps/header/WorkHeader';
 import KalmMobile from './../../assets/kalm/kalm-mobile.png';
 
 export default function Project() {
